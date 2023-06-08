7aedb323601ef3d13409f77cf5da0286c7fdb5c6
Katie Labgold <57493081+Katie-Labgold@users.noreply.github.com>
2023-06-08 17:14:51 -0400

    adding r scripts that prepared the example data

---
+++ data-raw/data-merge.R
@@ -0,0 +1,99 @@
+#---------------------------------------#
+# Testing data merges w/ and w/o complete territory data 
+#
+# K Labgold
+# 2023-06-01
+#---------------------------------------#
+
+#source("shapefile-prep.R")
+
+########################################################-
+# 1) First using data from 2020 Decennial Census ----
+########################################################-
+
+# Download and prep data for rbind
+acs5 <- load_variables(year = 2020, "acs5")
+dpvi <- load_variables(year = 2020, "dpvi")
+
+fips <- fips_codes %>% group_by(state) %>% slice(1)
+
+vars.acs <- c("B18135_002", "B18135_007", "B18135_012") # Estimate!!Total:!!Under 19 years:
+vars.dpvi <- c("DP3_0071P") #Percent!!HEALTH INSURANCE COVERAGE STATUS!!Civilian population under 19 years in households!!No health insurance coverage
+
+states <- get_acs(year = 2020, output = "wide", variables = vars.acs, 
+                        geography = "state", sumfile = "acs5") %>% # includes PR
+                    left_join(fips, by = c("NAME" = "state_name")) %>%
+                    mutate(
+                      "Percent Ages 19 or Under with No Insurance" = round(((B18135_007E + B18135_012E)/B18135_002E)*100, 1),
+                    STUSPS = state) %>%
+                    dplyr::select(GEOID, NAME, `Percent Ages 19 or Under with No Insurance`, STUSPS)
+  
+VI <- get_decennial(year = 2020, output = "wide", variables = vars.dpvi, state = "VI", 
+                        geography = "state", sumfile = "dpvi") %>%
+                    mutate(STUSPS = "VI") %>%
+                    rename("Percent Ages 19 or Under with No Insurance" = "DP3_0071P")
+
+GU <- get_decennial(year = 2020, output = "wide", variables = vars.dpvi, state = "GU", 
+                    geography = "state", sumfile = "dpgu") %>%
+                    mutate(STUSPS = "GU") %>%
+                    rename("Percent Ages 19 or Under with No Insurance" = "DP3_0071P")
+
+MP <- get_decennial(year = 2020, output = "wide", variables = vars.dpvi, state = "MP", 
+                    geography = "state", sumfile = "dpmp") %>%
+                    mutate(STUSPS = "MP") %>%
+                    rename("Percent Ages 19 or Under with No Insurance" = "DP3_0071P")
+
+AS <- get_decennial(year = 2020, output = "wide", variables = vars.dpvi, state = "AS", 
+                    geography = "state", sumfile = "dpas") %>%
+                    mutate(STUSPS = "AS") %>%
+                    rename("Percent Ages 19 or Under with No Insurance" = "DP3_0071P")
+
+# Rbind
+census <- states %>%
+            rbind(VI) %>%
+            rbind(GU) %>%
+            rbind(MP) %>%
+            rbind(AS) %>%
+            # Create Categorical Percent 
+            mutate(Percent.Cat = case_when(
+              `Percent Ages 19 or Under with No Insurance` < 5 ~ "Less than 5%",
+              `Percent Ages 19 or Under with No Insurance` >= 5 & `Percent Ages 19 or Under with No Insurance` < 10 ~ "5% to <10%",
+              `Percent Ages 19 or Under with No Insurance` >= 10 ~ "10% or Greater",
+            ),
+            Percent.Cat = factor(Percent.Cat, levels = c("Less than 5%", "5% to <10%", "10% or Greater")))
+
+rm(states, VI, GU, MP, acs5, dpvi, AS, vars.acs, vars.dpvi, oconus.list)
+
+census.uninsured19 <- census
+
+# Join with all geo
+
+all.geo.census <- all.geo %>%
+                    left_join(census, by = c("STUSPS"))
+
+
+########################################################-
+# 2) Using CDC downloaded data - missing territories ----
+########################################################-
+
+## CDC Wonder: Chronic Disease Indicators Cardiovascular Disease
+## https://nccd.cdc.gov/cdi/rdPage.aspx?rdReport=DPH_CDI.ExploreByTopic&islTopic=CVD&islYear=9999&go=GO
+
+cdc.cvd <- rio::import("cdcwonder_cardiovasculardisease.csv") %>%
+            filter(!is.na(ID), # remove empty rows
+                   LocationAbbr != "US") %>% # remove total US value
+            dplyr::select(ID, Description, LocationAbbr, LocationDesc, DataSource,
+                          Topic, Data_Value_Unit, Data_Value_Type, Data_Value,
+                          Low_Confidence_Limit, High_Confidence_Limit)  %>%
+            mutate(data.cat = case_when(
+              Data_Value < 198 ~ "Q1 (166 to < 198)",
+              Data_Value >= 198 & Data_Value < 215 ~ "Q2 (198 to < 215)",
+              Data_Value >= 215 & Data_Value < 248 ~ "Q3 (215 to < 248)",
+              Data_Value >= 248 & Data_Value < 400 ~ "Q4 (248 to 326)",
+            ))
+
+
+# Merge cdc data w/ all.geo file
+all.geo.cdc <- all.geo %>%
+                left_join(cdc.cvd, by = c("STUSPS" = "LocationAbbr"))
+
---
+++ data-raw/shapefile-prep.R
@@ -0,0 +1,118 @@
+#---------------------------------------#
+# Download and Prepare Shapefiles at State/Territory Level
+#
+# K Labgold
+# 2023-05-11
+#---------------------------------------#
+
+# Details ----
+## U.S. Census Bureau provides shapefiles for:
+## U.S. States + D.C.
+## Five territories: Puerto Rico (PR), U.S. Virgin Islands (USVI), Guam, American Samoa, Commonwealth of Northern Mariana Islands (CNMI)
+
+### KL note to self: Check for freely associated states:
+### Federated States of Micronesia (FSM), Republic of the Marshall Islands (RMI), and Republic of Palau (Palau) 
+
+
+pacman::p_load(dplyr, ggplot2, sf, tidycensus, stringr, extrafont, cowplot, grid)
+`%notin%` <- Negate(`%in%`)
+
+us <- st_read("cb_2018_us_state_500k.shp") # manual download, since tigris package included water areas
+
+# Some localities are going to need parsed geographies, downloading and formatting these separately using tidycensus
+HI <- get_decennial(geography = "tract", variables = "H001001", state = "HI", sumfile = "sf1", geometry = TRUE) %>%
+            filter(value != 0) %>%
+            dplyr::mutate(co = substr(GEOID, 3, 5),
+                          st = substr(GEOID, 1, 2),
+                          STUSPS = "HI") %>%
+            dplyr::select(st, STUSPS) %>%
+            group_by(st, STUSPS) %>%
+            dplyr::summarise() %>%
+            ungroup() %>%
+            mutate(group = "HI")
+
+VI.stx <- get_decennial(year = 2020, output = "wide", variables = "DP1_0001C", state = "VI", 
+                       geography = "county", geometry = TRUE, sumfile = "dpvi") %>%
+                       dplyr::filter(GEOID == 78010) %>%
+                        ungroup() %>%
+                        mutate(st = substr(GEOID, 1, 2),
+                               STUSPS = "VI") %>%
+                        dplyr::select(st, STUSPS) %>%
+                        mutate(group = "VI.stx")
+
+VI.stt_stj <- get_decennial(year = 2020, output = "wide", variables = "DP1_0001C", state = "VI", 
+                        geography = "county", geometry = TRUE, sumfile = "dpvi") %>%
+                        filter(GEOID != 78010) %>%
+                        ungroup() %>%
+                        mutate(st = substr(GEOID, 1, 2),
+                               STUSPS = "VI") %>%
+                        dplyr::select(st, STUSPS) %>%
+                        mutate(group = "VI.stt_stj")
+
+AS <- get_decennial(year = 2020, output = "wide", variables = "DP1_0001C", 
+                       state = "AS", geography = "county", geometry = TRUE, sumfile = "dpas") %>%
+                       filter(DP1_0001C != 0) %>%
+                       mutate(st = substr(GEOID, 1, 2),
+                              STUSPS = "AS") %>%
+                       dplyr::select(st, STUSPS) %>%
+                       group_by(st, STUSPS) %>%
+                       dplyr::summarise() %>%
+                        ungroup() %>%
+                        mutate(group = "AS")
+
+MP <- get_decennial(year = 2020, output = "wide", variables = "DP1_0001C", state = "MP", 
+                       geography = "county", geometry = TRUE, sumfile = "dpmp") %>%
+                       filter(GEOID != 69085) %>%
+                       mutate(st = substr(GEOID, 1, 2),
+                              STUSPS = "MP") %>%
+                      dplyr::select(st, STUSPS) %>%
+                       group_by(st, STUSPS) %>%
+                       dplyr::summarise() %>%
+                       ungroup() %>%
+                       mutate(group = "MP")
+
+
+# Separate out CONUS and OCONUS locales ----
+oconus.list <- c("HI", "AK", "VI", "PR", "GU", "AS", "MP")
+
+mainland <- us %>% 
+              filter(STUSPS %notin% oconus.list) %>%
+              #st_transform(5070) %>%  # bring back in the map portion
+              dplyr::select(STATEFP, STUSPS) %>%
+              rename("st" = "STATEFP") %>%
+              mutate(group = "mainland")
+
+AK <- us %>% 
+      filter(STUSPS == "AK") %>% 
+      #st_transform(3338) %>%  # bring back in the map portion
+      dplyr::select(STATEFP, STUSPS) %>%
+      rename("st" = "STATEFP") %>%
+      mutate(group = "AK")
+
+PR <- us %>% 
+        filter(STUSPS == "PR") %>%
+        dplyr::select(STATEFP, STUSPS) %>%
+        rename("st" = "STATEFP") %>%
+        mutate(group = "PR")
+        
+GU <- us %>% 
+      filter(STUSPS == "GU") %>% 
+      dplyr::select(STATEFP, STUSPS) %>%
+      rename("st" = "STATEFP") %>%
+      mutate(group = "GU")
+
+
+# Prepare overarching file: ----
+all.geo <- mainland %>%
+            rbind(HI) %>%
+            rbind(AK) %>%
+            rbind(PR) %>%
+            rbind(GU) %>%
+            rbind(MP) %>%
+            rbind(AS) %>%
+            rbind(VI.stx) %>% #later use starts with "VI."
+            rbind(VI.stt_stj)
+            
+rm(mainland, HI, AK, PR, GU, MP, AS, VI.stx, VI.stt_stj)
+
+rm(us)
