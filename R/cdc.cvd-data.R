2e43b84a949a934b596abafaf8c6d3c1af7cc59d
Katie Labgold <57493081+Katie-Labgold@users.noreply.github.com>
2023-06-22 10:26:26 -0400

    Adding data documentation for cdc.cvd

---
+++ R/cdc.cvd-data.R
@@ -0,0 +1,23 @@
+#' CDC Cardiovascular Mortality Rate Data for 50 US States and D.C.
+#' 
+#' Data from CDC Wonder Chronic Disease Indicators Cardiovascular Disease.
+#' Specifically, 2020 age-adjusted mortality rate from total cardiovascular disease per 100,000.
+#' 
+#' @docType data
+#' 
+#' @usage data(cdc.cvd)
+#' 
+#' @format An object of class
+#' 
+#' @keywords datasets
+#' 
+#' @references Centers for Disease Control and Prevention, National Center for Chronic Disease Prevention and Health Promotion, Division of Population Health. Chronic Disease Indicators (CDI) Data [online]. [accessed Jun 22, 2023]. URL: https://nccd.cdc.gov/cdi.
+#' 
+#' @source \href{https://nccd.cdc.gov/cdi/rdPage.aspx?rdReport=DPH_CDI.ExploreByTopic&islTopic=CVD&islYear=9999&go=GO}{CDC Wonder}
+#' 
+#' @examples
+#' data(cdc.cvd)
+#' class(cdc.cvd$data.cat)
+#' table(cdc.cvd$data.cat)
+#' head(cdc.cvd$LocationAbbr)
+"cdc.cvd"
