ce33ffe2f78d5eb37f46dc539a2b8d37e64918dd
Katie Labgold <57493081+Katie-Labgold@users.noreply.github.com>
2024-07-02 16:25:56 -0400

    adding test file during dev

---
+++ data-raw/fun_test.R
@@ -0,0 +1,23 @@
+# Function Test File
+
+colors.census <- c("Less than 5%" = "#feebe2", 
+                    "5% to <10%" = "#f768a1", 
+                   "10% or Greater" = "#7a0177")
+
+
+map1_categorical_county(data = census.uninsured19.co, 
+                        join_var = "GEOID", 
+                        fill_var = "Percent.Cat", 
+                        fill_color = colors.census, 
+                        fill_linewidth = 0.5, 
+                        fill_linecolor = "gray50",
+                        legend_name = "Percent Uninsured",
+                        title = "Figure 1. Percent Uninsured, Ages <19 Years",
+                        #border_ids = border,
+                        #border_color = "red",
+                        #border_linewidth = 0.5,
+                      #state_color = "black", 
+                      #state_linewidth = 0.75,
+                        save.filepath = "saved-maps/map1-test-co.png")
+
+
