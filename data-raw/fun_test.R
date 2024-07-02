ecc7b34938d791042399b088454c4f2e459b4ba5
Katie Labgold <57493081+Katie-Labgold@users.noreply.github.com>
2024-07-02 17:10:56 -0400

    adding co borders for test

--- data-raw/fun_test.R
+++ data-raw/fun_test.R
@@ -4,6 +4,7 @@ colors.census <- c("Less than 5%" = "#feebe2",
                     "5% to <10%" = "#f768a1", 
                    "10% or Greater" = "#7a0177")
 
+border <- c("13031", "13089", "13121")
 
 map1_categorical_county(data = census.uninsured19.co, 
                         join_var = "GEOID", 
@@ -13,11 +14,11 @@ map1_categorical_county(data = census.uninsured19.co,
                         fill_linecolor = "gray50",
                         legend_name = "Percent Uninsured",
                         title = "Figure 1. Percent Uninsured, Ages <19 Years",
-                        #border_ids = border,
-                        #border_color = "red",
-                        #border_linewidth = 0.5,
-                      state_color = "black", 
-                      state_linewidth = 1,
-                        save.filepath = "saved-maps/map1-test-co.png")
+                        border_ids = border,
+                        border_color = "red",
+                        border_linewidth = 0.5,
+                        state_color = "black", 
+                        state_linewidth = 1,
+                        save.filepath = "saved-maps/map1-test-co-highlight.png")
 
 
